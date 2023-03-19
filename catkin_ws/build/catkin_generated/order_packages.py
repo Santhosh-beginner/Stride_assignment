e463602ec9c932229daa75ae56f458856b687107
Santhosh <rsanthoshreddy135@gmail.com>
2023-03-19 19:21:41 +0530

    initial commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/santhosh/stride_assignment/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/santhosh/stride/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/santhosh/stride/catkin_ws/devel;/opt/ros/noetic' != '' else []
